ac2cd7950da968197512ae60ffa32fea33dcd649
jwgreer <124017623+jwgreer@users.noreply.github.com>
2023-04-01 12:30:32 -0600

    Update clsMainLogic.cs

--- Main/clsMainLogic.cs
+++ Main/clsMainLogic.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace GroupProject.Main
 {
-    internal class clsMainLogic
+    class clsMainLogic
     {
+        /// <summary>
+    /// Main logic class for main window
+    /// This will contain the methods for connecting to the DB
+    /// Contains buttons to Create, save, and edit an invoice
+    /// Item list will need to be updated anytime the page loads
+    /// Invoices edited or created will get added to the invoice list after being saved
+    /// Datagrid will display items on invoice
+    /// </summary>
+    class clsMainLogic
+    {
+        // invoice class
+        // item class
+        // invoice class list
+        // list <clsInvoice>
+        // item class list
+        // item <clsItems>
+
+        //string to conncet to DB
+
+
+        //GetAllItems
+        //SaveNewInvoice
+        //EditInvoice
+        //GetInvoice
+
+        //When Item window is closed, on load Main window will need to refresh combo boxes with items in it
+        //cmbItem Refreshonload
+
+    }
     }
 }
