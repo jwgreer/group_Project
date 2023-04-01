bd9ecc5db275f4945f7e583f4d50bd12a222e4d3
allansantos7 <allansantos@mail.weber.edu>
2023-04-01 01:16:28 -0600

    Add project files.

---
+++ Search/clsSearchLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.Search
+{
+    /// <summary>
+    /// Business Logic class for the Search Window
+    /// Contains methods for connecting to the DB
+    /// Contains search/filter methods for Invoices from the DB
+    /// Queries the DB using InvoiceID/InvoiceDate/InvoiceCost passed into the SQL statements in clsSearchSQL
+    /// Any Invoices returned from the Queries is added to a list.
+    /// List is then returned to the top level method called by the Search Window.
+    /// </summary>
+    class clsSearchLogic
+    {
+        // Invoice class
+        // clsInvoice invoice
+        // Invoice class List
+        // List<clsInvoices>
+
+        /// <summary>
+        /// Connection string to the DB
+        /// </summary>
+        private string sConnectionString;
+
+        clsSearchLogic() 
+        {
+            // Connect to the "mdb" database
+            sConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data source= " + Directory.GetCurrentDirectory() + "\\Invoice.mdb";
+        }
+
+        // Method for executing the passed in SQL statement
+        // public DataSet ExecuteSQLStatement(string sSQL)
+
+        // GetDistinctInvoiceNumber
+        // GetDistinctInvoiceDates
+        // GetDistinctInvoiceCharges
+        // GetAllInvoices() - returns List<clsInvoices>
+        // GetInvoices(InvoiceNumber, InvoiceDate, TotalCost) - returns List<clsInvoices>
+    }
+}
---
+++ Search/wndSearch.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GroupProject.Search
+{
+    /// <summary>
+    /// Interaction logic for Search Window
+    /// Once the Search Window opens, displays all invoices into the Data Grid using a list of invoice objects.
+    /// Search window allows the user to search Invoices based on a few filters selected in a dropdown box:
+    /// Invoice Number, Invoice Date, and Total Cost
+    /// The user may use use each filter individually or may use invoice date and total Cost filters together.
+    /// Once the user has selected an invoice, they can close the Search Window and by using a local variable that selected invoice gets passed to the Main Window for viewing.
+    /// The user may also reset the search window to its inital state by selecting the respective button at any time.
+    /// </summary>
+    public partial class wndSearch1 : Window
+    {
+        public wndSearch1()
+        {
+            InitializeComponent();
+            // Method for displaying all invoices from clsSearch.cs
+        }
+
+        // sSelectedInvoiceID - Local variable. If a user has selected an invoice in the Data grid store that Invoice Num, otherwise variable is empty.
+
+        // SelectedInvoiceID - Property main window can access to get the selected invoice ID
+
+        /// <summary>
+        /// Method for the Invoice Number dropdown box selection.
+        /// When a user selects an invoice number, filter the results in the Invoice Data Grid by the chosen number
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Method for Invoice Date dropdown box selection
+        /// When a user selects an invoice by date, filter the results in the Invoice Data Grid by that date.
+        /// May work alongside with Invoice Total Cost dropdown box selection for filtering results. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbxInvoiceDate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Method for Invoice Total Cost dropdown box selection
+        /// When a user selects an invoice by the total Cost, filter the results in the Invoice Data Grid by that amount,
+        /// Sorted from smalled to largest.
+        /// May work alongside with Invoice Date dropdown box selection for filtering results.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbxInvoiceTotal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Method for selecting an invoice for viewing on the main screeen.
+        /// As long as a user has selected an invoice in the Invoice Data Grid,
+        /// Pass that invoice number using the local variable to the main window for viewing.
+        /// The search window is also hidden.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInvoiceSelect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Method for reseting the search window to its initial state.
+        /// When a user presses this button,
+        /// Clear the Invoice Data Grid of any selection and
+        /// all dropdown boxes are reset to have unselected values.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInvoiceClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Method for canceling the invoice search.
+        /// When pressed, Cancels the search by hiding the Search Window to return to the Main window.
+        /// Nothing is passed to the Main Window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInvoiceCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
